8f58e85f963650b5dff71580267ddb9f35c402a8
SamuelKahler <samuelkahler@yahoo.com>
2026-07-20 17:37:18 -0700

    Delete sports-edge-clean-repo copy directory

--- sports-edge-clean-repo copy/sql/0002_live_game_metadata.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "live_games" ADD COLUMN IF NOT EXISTS "home_team_name" text DEFAULT '';
-ALTER TABLE "live_games" ADD COLUMN IF NOT EXISTS "away_team_name" text DEFAULT '';
-ALTER TABLE "live_games" ADD COLUMN IF NOT EXISTS "home_pitcher" text DEFAULT 'TBD';
-ALTER TABLE "live_games" ADD COLUMN IF NOT EXISTS "away_pitcher" text DEFAULT 'TBD';
-ALTER TABLE "live_games" ADD COLUMN IF NOT EXISTS "game_time" text DEFAULT '';
